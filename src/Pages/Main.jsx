fd91361a55f464588006c9defd17937104f6b35a
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-06-10 23:07:29 +0530

    updated navbar and BannerPage

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,68 @@
+import BugBountyStats from "./BugBountyStats/BugBountyStats";
+import EarnMoney from "./EarnMoney/EarnMoney";
+import Partners from "./Partners/Partners";
+import HomePage from "./BannerPage/BannerPage";
+import SecureOrganization from "./SecureOrganization/SecureOrganization";
+import Team from "./Team/Team";
+import WhyJoin from "./WhyJoin/WhyJoin";
+import React, { useEffect } from "react";
+
+import "./style.css";
+
+const Main = () => {
+  useEffect(() => {
+    const handleScrollToSection = () => {
+      const path = window.location.hash.substring(1);
+      const targetSection = document.getElementById(path);
+      if (targetSection) {
+        setTimeout(() => {
+          const sectionOffset = 100;
+          const offsetTop = targetSection.offsetTop - sectionOffset;
+          window.scrollTo({ top: offsetTop, behavior: "smooth" });
+        }, 10); // Delay the calculation by 100 milliseconds
+      }
+    };
+
+    handleScrollToSection(); // Scroll to section on initial load
+
+    window.addEventListener("hashchange", handleScrollToSection);
+
+    return () => {
+      window.removeEventListener("hashchange", handleScrollToSection);
+    };
+  }, []);
+
+  return (
+    <div>
+      <section id="HomePage">
+        <HomePage />
+      </section>
+
+      <section id="Partners">
+        <Partners />
+      </section>
+
+      <section id="WhyJoin">
+        <WhyJoin />
+      </section>
+
+      <section id="SecureOrganization">
+        <SecureOrganization />
+      </section>
+
+      <section id="BugBountyStats">
+        <BugBountyStats />
+      </section>
+
+      <section id="EarnMoney">
+        <EarnMoney />
+      </section>
+
+      <section id="Team">
+        <Team />
+      </section>
+    </div>
+  );
+};
+
+export default Main;
