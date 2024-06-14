22f6d3b3b481dd9ea304fd87f4cad584e5bc6008
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-06-14 10:52:24 +0530

    updated new navBar

--- src/components/Pages/Main.jsx
+++ src/components/Pages/Main.jsx
@@ -67,7 +67,7 @@ const Main = () => {
 
   return (
     <div>
-      {/* <HomePageNavBar /> */}
+      <HomePageNavBar />
       <section id="HomePage">
         <HomePage />
       </section>
--- src/components/Reusable/NavBar/HomePageNavBar/HomePageNavBar.jsx
+++ src/components/Reusable/NavBar/HomePageNavBar/HomePageNavBar.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Menu, Segment } from "semantic-ui-react";
 import style from "./HomePageNavBar.module.css";
 
 const HomePageNavBar = () => {
   const [activeItem, setActiveItem] = useState("home");
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsScrolled(false);
+      } else {
+        setIsScrolled(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Segment inverted className={style.navbar_segment}>
+    <Segment
+      inverted
+      className={`${style.navbar_segment} ${
+        isScrolled ? style.visible : style.hidden
+      }`}
+    >
       <Menu.Item as="a" className={style.navbar_logo}>
         Bug Zero
       </Menu.Item>
