7ffaf57c006f20cf13286b83d0332fef804da90d
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-06-19 19:43:38 +0530

    Update BlogPage.jsx

--- src/components/Pages/BlogPage/BlogPage.jsx
+++ src/components/Pages/BlogPage/BlogPage.jsx
@@ -7,26 +7,57 @@ import {
   Card,
   Icon,
   Image,
+  Segment,
 } from "semantic-ui-react";
-import test from "../../../Assets/logo/bhasha.png";
+import testimge from "../../../Assets/logo/bhasha.png";
 
-const CardExampleImageCard = () => (
-  <Card>
-    <Image src={test} wrapped ui={false} />
-    <CardContent>
-      <CardHeader>Daniel</CardHeader>
-      <CardMeta>Joined in 2016</CardMeta>
-      <CardDescription>
-        Daniel is a comedian living in Nashville.
-      </CardDescription>
-    </CardContent>
-    <CardContent extra>
-      <a>
-        <Icon name="user" />
-        10 Friends
-      </a>
-    </CardContent>
-  </Card>
-);
+const CardExampleImageCard = () => {
+  const test = [
+    {
+      image: testimge,
+      title: "testing cat ",
+      author: "induranga",
+      date: "2024/05/15",
+    },
+    {
+      image: testimge,
+      title: "testing cat ",
+      author: "induranga",
+      date: "2024/05/15",
+    },
+    {
+      image: testimge,
+      title: "testing cat ",
+      author: "induranga",
+      date: "2024/05/15",
+    },
+    {
+      image: testimge,
+      title: "testing cat ",
+      author: "induranga",
+      date: "2024/05/15",
+    },
+  ];
+  return (
+    <Segment>
+      {test.map((item) => (
+        <Card>
+          <Image src={item.image} wrapped ui={false} />
+          <CardContent>
+            <CardHeader>{item.title}</CardHeader>
+            <CardMeta>{`Author ${item.author}`}</CardMeta>
+            <CardMeta>{`Publish on ${item.date}`}</CardMeta>
+          </CardContent>
+          <CardContent extra>
+            <a>
+              <Icon name="user" />
+              10 Friends
+            </a>
+          </CardContent>
+        </Card>
+      ))}
+    </Segment>
+  );
+};
 
 export default CardExampleImageCard;
