f2c9d379ad8c416b368a1cb47dfec0a36d9d1214
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-06-21 18:28:44 +0530

    Update BlogPage.jsx

--- src/components/Pages/BlogPage/BlogPage.jsx
+++ src/components/Pages/BlogPage/BlogPage.jsx
@@ -8,6 +8,8 @@ import {
   Icon,
   Image,
   Segment,
+  Container,
+  Header,
 } from "semantic-ui-react";
 import testimge from "../../../Assets/logo/bhasha.png";
 
@@ -39,7 +41,15 @@ const CardExampleImageCard = () => {
     },
   ];
   return (
-    <Segment>
+    <Segment
+      style={{ padding: "6em 0em", border: "none", backgroundColor: "#212121" }}
+    >
+      <Container text>
+        <Header style={{ fontSize: "2.5em", color: "#F4C700" }}>
+          Expore Our Knowledge Base
+        </Header>
+        <br></br>
+      </Container>
       <Grid container doubling columns={4}>
         {test.map((item, index) => (
           <Grid.Column
@@ -55,11 +65,17 @@ const CardExampleImageCard = () => {
                 paddingTop: "10%",
                 display: "flex",
                 alignItems: "center",
+                border: "3px , solid ,#F4C700",
+                color: "white",
+                // backgroundColor: "#F4C700",
+                // opacity: "0.6",
               }}
             >
               <Segment
                 style={{
-                  width: "50%",
+                  width: "90%",
+                  borderColor: "none",
+                  padding: "none",
                 }}
               >
                 <Image src={item.image} alt="Prof Kasun De Zoysa" />
