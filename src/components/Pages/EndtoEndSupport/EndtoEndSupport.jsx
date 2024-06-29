80835fcde0b5bd734be7fe612c63279c01d16d37
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-06-30 02:40:12 +0530

    updated endtoendPage

--- src/components/Pages/EndtoEndSupport/EndtoEndSupport.jsx
+++ src/components/Pages/EndtoEndSupport/EndtoEndSupport.jsx
@@ -1,66 +1,34 @@
 import React from "react";
-import { GridColumn, Grid, Container, Header, Image } from "semantic-ui-react";
+import { Segment, Container, Header } from "semantic-ui-react";
+import { Stepper } from "react-form-stepper";
 
-import clock from "../../../Assets/numbers/number-one.png";
-import equalizer from "../../../Assets/numbers/number-2.png";
-import iteration from "../../../Assets/numbers/number-3.png";
-import guru from "../../../Assets/numbers/number-four.png";
-import "./endtoend.css";
-
-const Step = ({ img, title }) => (
-  <GridColumn>
-    <div class="ui steps" style={{ display: "block", margin: "auto" }}>
-      <div
-        class="step step-card"
-        style={{ padding: "2em 1em", justifyContent: "center" }}
-      >
-        <Image
-          src={img}
-          alt="BigCo Inc. logo"
-          size="tiny"
-          style={{ margin: "auto" }}
-        />
-        <div class="content" style={{ margin: "auto" }}>
-          <div
-            class="title title-margin"
-            style={{
-              textAlign: "center",
-              lineHeight: "22px",
-              fontSize: "1em",
-            }}
-          >
-            {title}
-          </div>
-          {/* <div class="description" style={{ margin: "0", fontSize: "10px" }}>
-            {desc}
-          </div> */}
-        </div>
-      </div>
-    </div>
-  </GridColumn>
-);
-
-const EndtoEndSupport = () => (
-  <>
-    <Container text>
-      <Header as="h3" style={{ fontSize: "2.5em", color: "#f4c700" }}>
-        Providing end to end support to every bug bounty program
-      </Header>
-      <br></br>
-    </Container>
-    <Grid
-      container
-      stackable
-      columns={4}
-      style={{ backgroundcolor: " #f0f0f0" }}
+const EndtoEndSupport = () => {
+  return (
+    <Segment
+      style={{
+        padding: "6em 0em",
+        border: "none",
+        backgroundColor: "transparent",
+      }}
     >
-      <Step img={clock} title="Define policy Scope and Rewards" />
-      <Step img={equalizer} title="Define Right Tester" />
-      <Step img={iteration} title="Validate and prioratize vulnalabilities" />
-      <Step img={guru} title="Verify and Intergrate" />
-    </Grid>
-    <br />
-    <br />
-  </>
-);
+      <Container text>
+        <Header as="h3" style={{ fontSize: "2.5em", color: "#f4c700" }}>
+          Providing end to end support to every bug bounty program
+        </Header>
+        <br></br>
+      </Container>
+
+      <Stepper
+        steps={[
+          { label: "Define policy Scope and Rewards" },
+          { label: "Define Right Tester" },
+          { label: "Validate and prioratize vulnalabilities" },
+          { label: "Verify and Intergrate" },
+        ]}
+        activeStep={4}
+      />
+    </Segment>
+  );
+};
+
 export default EndtoEndSupport;
