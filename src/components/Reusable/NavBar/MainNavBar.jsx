9fe4b1b2d16b8e86f83f9d40e29b3c5b7d0bfc48
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-08-08 19:51:48 +0530

    updated css

--- src/components/Reusable/NavBar/MainNavBar.jsx
+++ src/components/Reusable/NavBar/MainNavBar.jsx
@@ -119,10 +119,14 @@ const DesktopContainer = ({ children, activeSection }) => {
               <Menu.Item position="right">
                 <Button
                   as="a"
-                  inverted
                   color="yellow"
-                  style={{ borderRadius: "25px" }}
-                  basic
+                  inverted
+                  style={{
+                    borderRadius: "25px",
+                    border: "none",
+                    color: "#212121",
+                    backgroundColor: "#f4c700",
+                  }}
                   href={`${process.env.PUBLIC_URL}/signin`}
                 >
                   {/* <a href="http://localhost:3000/bz-test-app/signin"></a> */}
