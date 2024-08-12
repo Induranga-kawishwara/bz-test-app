101547742aceec313e521f0f2285890eea64b314
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-08-12 11:57:33 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.css";
 import "semantic-ui-less/semantic.less";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.Fragment>
     <App />
-  </React.Fragment>,
-  document.getElementById("root")
+  </React.Fragment>
 );
